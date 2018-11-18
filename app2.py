b64cbd68164a27ad8c9bfc4c3b1e87c605428617
naoya0082 <6ds3f7@gmail.com>
2018-11-18 10:44:40 +0900

    app2.py にて warikan2 の関数「warikan」をimport 関数を引き継げる

---
+++ app2.py
@@ -0,0 +1,4 @@
+# 割り勘計算したい 7000円で6人
+from warikan2 import calculate_warikan
+
+print(calculate_warikan(7000, 6))
--- warikan2.py
+++ warikan2.py
@@ -1,7 +1,6 @@
-def warikan(amount, number_of_people):
+def calculate_warikan(amount, number_of_people):
     return f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円"
 
-
-print(warikan(1500, 3))
-print(warikan(2000, 3))
-print(warikan(3000, 4))
+# print(warikan(1500, 3))
+# print(warikan(2000, 3))
+# print(warikan(3000, 4))
