7446f4c2d1e39bb2ae8143b533d8fde5480e0c1f
naoya0082 <6ds3f7@gmail.com>
2018-11-18 09:41:40 +0900

    3table分の割り勘を計算

--- app.py
+++ app.py
@@ -7,16 +7,12 @@ hello()
 
 def say_hello(name):
     print("Hi " + name)
-
-
 say_hello("Bob")
 say_hello("Naoya")
 
 
 def double(number):
     return 2 * number
-
-
 double(3)
 result = double(3)
 print(result)
@@ -25,7 +21,5 @@ print(result)
 # 1分課題
 def str_combine(str1, str2):
     return str1 + str2
-
-
 result = str_combine("kazma", "takahashi")  # kazma takahashi を返す
 print(result)
---
+++ warikan.py
@@ -0,0 +1,21 @@
+def table1():
+    amount = 1500
+    number_of_people = 3
+    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+
+def table2():
+    amount = 2000
+    number_of_people = 3
+    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+
+def table3():
+    amount = 3647
+    number_of_people = 4
+    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+
+table1()
+table2()
+table3()
