c6a2f55364bfba673fbc57bd2e83acbf918ea190
naoya0082 <6ds3f7@gmail.com>
2018-11-18 10:11:53 +0900

    warikanを簡易化 関数化

--- warikan.py
+++ warikan.py
@@ -1,21 +1,33 @@
-def table1():
-    amount = 1500
-    number_of_people = 3
-    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+# def table1():
+#     amount = 1500
+#     number_of_people = 3
+#     print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+#
+#
+# def table2():
+#     amount = 2000
+#     number_of_people = 3
+#     print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+#
+#
+# def table3():
+#     amount = 3647
+#     number_of_people = 4
+#     print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+#
+#
+# table1()
+# table2()
+# table3()
 
 
-def table2():
-    amount = 2000
-    number_of_people = 3
-    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+def warikan(amount, number_of_people):
+    return f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円"
 
 
-def table3():
-    amount = 3647
-    number_of_people = 4
-    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
-
-
-table1()
-table2()
-table3()
+result1 = warikan(1500, 3)
+result2 = warikan(2000, 3)
+result3 = warikan(3647, 4)
+print(result1)
+print(result2)
+print(result3)
---
+++ warikan2.py
@@ -0,0 +1,7 @@
+def warikan(amount, number_of_people):
+    return f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円"
+
+
+print(warikan(1500, 3))
+print(warikan(2000, 3))
+print(warikan(3000, 4))
